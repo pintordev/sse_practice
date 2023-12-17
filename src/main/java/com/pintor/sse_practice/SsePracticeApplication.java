1672643f860309c0fef8c594dd647da091c6cda8
pintor.dev <pintor.dev@gmail.com>
2023-12-17 15:19:06 +0900

    #1 - 프로젝트 세팅

---
+++ src/main/java/com/pintor/sse_practice/SsePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pintor.sse_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pintor/sse_practice/SsePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintor.sse_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
